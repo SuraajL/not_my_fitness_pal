7485e8ffeb6b0bdbe0dc0d861d845a17a8972688
Nasir <nasir-islam@hotmail.com>
2022-02-20 17:04:10 +0000

    add testing for Person and Food

--- src/main/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplication.java
+++ src/main/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplication.java
@@ -1,5 +1,9 @@
 package com.group6.not_my_fitness_pal;
 
+import com.group6.not_my_fitness_pal.food.Day;
+import com.group6.not_my_fitness_pal.food.Food;
+import com.group6.not_my_fitness_pal.food.MealType;
+import com.group6.not_my_fitness_pal.person.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +13,12 @@ public class NotMyFitnessPalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotMyFitnessPalApplication.class, args);
 		System.out.println("Hello World");
+
+		Person test = new Person(1,"me", 10, 168.8, 65.5, 2600);
+		System.out.println(test);
+
+		Food foodTest = new Food(1, 1, "Borgor", MealType.BREAKFAST, "I dont know why", 600, 1, Day.SUNDAY);
+		System.out.println(foodTest);
 	}
 
 }
