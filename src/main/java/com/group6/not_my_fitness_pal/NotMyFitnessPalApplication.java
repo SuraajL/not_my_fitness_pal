d28637d0c36f7799014a71261a165740961bbe78
Nasir <nasir-islam@hotmail.com>
2022-02-20 16:09:49 +0000

    initial commit

---
+++ src/main/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplication.java
@@ -0,0 +1,13 @@
+package com.group6.not_my_fitness_pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotMyFitnessPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotMyFitnessPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.not_my_fitness_pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotMyFitnessPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
