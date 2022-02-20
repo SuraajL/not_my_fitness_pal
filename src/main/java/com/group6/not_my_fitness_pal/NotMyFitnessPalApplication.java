356f44e1a67fabad13ad43aa9faf61dcc46eff34
Nasir <nasir-islam@hotmail.com>
2022-02-20 16:17:06 +0000

    comments out postgress and jdbc dependencies in pom.xml

--- src/main/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplication.java
+++ src/main/java/com/group6/not_my_fitness_pal/NotMyFitnessPalApplication.java
@@ -8,6 +8,7 @@ public class NotMyFitnessPalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotMyFitnessPalApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
