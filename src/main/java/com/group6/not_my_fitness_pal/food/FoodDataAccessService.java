3972a44d97bf7b5a1eb1855ff3cd9185375ea6f9
Nasir <nasir-islam@hotmail.com>
2022-02-21 21:59:19 +0000

    add addFood postgres implementation

--- src/main/java/com/group6/not_my_fitness_pal/food/FoodDataAccessService.java
+++ src/main/java/com/group6/not_my_fitness_pal/food/FoodDataAccessService.java
@@ -1,5 +1,6 @@
 package com.group6.not_my_fitness_pal.food;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -7,9 +8,34 @@ import java.util.List;
 @Repository("food_postgres")
 public class FoodDataAccessService implements FoodDao{
 
+    private JdbcTemplate jdbcTemplate;
+
+    public FoodDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int addFood(Food food) {
-        return 0;
+        // THIS IS THE SQL IMPLEMENTATION
+        // Note the sql command doesn't need the id as it is a SERIAL and is generated automatically
+        String sql = """
+                INSERT INTO food_entries (person_id, name, meal_type, notes, calories, week, day)
+                VALUES(?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        // Use .update Method when Inserting/deleting/updating
+        int rowsAffected = jdbcTemplate.update(
+                sql,
+                food.getPerson_id(),
+                food.getName(),
+                food.getMealType().name(),
+                food.getNotes(),
+                food.getCalories(),
+                food.getWeek(),
+                food.getDay().name()
+        );
+        // Should return 1 as only 1 row is added - anything else it should throw error in the service!!
+        return rowsAffected;
     }
 
     @Override
