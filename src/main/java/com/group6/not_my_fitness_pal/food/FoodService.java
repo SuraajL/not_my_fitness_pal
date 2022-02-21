10aaae26b21075e08b22b5af6c9f7e60f797a694
Nasir <nasir-islam@hotmail.com>
2022-02-21 21:03:18 +0000

    add a little comment

--- src/main/java/com/group6/not_my_fitness_pal/food/FoodService.java
+++ src/main/java/com/group6/not_my_fitness_pal/food/FoodService.java
@@ -25,11 +25,11 @@ public class FoodService {
     //TODO:3) Create Psuedo code for add food
     public int addFoodEntry(Food food) {
         // Check all fields are valid (enums don't have to be checked here):
-        //    person_id - use person service, if null then throw exception - using PersonDao.
+        //    person_id - use person service, if null then throw exception - using PersonDao. (Mock)
         //    name - can't be null
         //    calories - can't be null or < 0 - 0 is accepted
         //    week - can't be null or <= 0
-        // Add food entry to sql db - using FoodDao.
+        // Add food entry to sql db - using FoodDao.  (Mock)
         // foodDao.addFood(someRandom)
         // If result != 1, then throw exception to say it failed
 
