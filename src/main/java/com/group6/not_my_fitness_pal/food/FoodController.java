1df5bdb1e683deb57649cf1ae25057689670219f
Sarina Salamon <salamon.sarina@gmail.com>
2022-02-22 11:26:47 +0000

    add getAllFood functionality

--- src/main/java/com/group6/not_my_fitness_pal/food/FoodController.java
+++ src/main/java/com/group6/not_my_fitness_pal/food/FoodController.java
@@ -1,8 +1,11 @@
 package com.group6.not_my_fitness_pal.food;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FoodController {
@@ -16,6 +19,11 @@ public class FoodController {
     @PostMapping(path = "food")
     public void addFood (@RequestBody Food food){
         foodService.addFoodEntry(food);
+    }
+
+    @GetMapping(path = "getAllFood")
+    public List<Food> foodList(){
+        return foodService.getAllFoodEntries();
     }
 
 }
